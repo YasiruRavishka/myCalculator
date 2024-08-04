e84fd8fd1ad4c6bc69fb8a7712a9b51770544261
Yasiru_Ravishka <YasiruRavishka47@gmail.com>
2024-08-04 19:00:54 +0530

    js improvement

--- app.js
+++ app.js
@@ -1,54 +1,111 @@
 const lightTheme = "style/light.css";
 const darkTheme = "style/dark.css";
-let displayText = "";
-let outputResult = 0;
-document.getElementById("displayArea").readOnly = true; //locked displayArea
+const displayText = document.getElementById("displayArea");
+displayText.readOnly = true; //locked displayArea
+let resetStatus = false;
+let isDecimalNumber = false;
+let digitCount = 0;
 
-function display() {
-  document.getElementById("displayArea").value = displayText;
+function calc() {
+  let result;
+  try {
+    result = eval(displayText.value);
+    if (!isFinite(result)) {  //check NaN, Infinity
+      throw SyntaxError;
+    }
+    if (Number.isInteger(result)) { //check float or integer
+      isDecimalNumber = false;
+    } else {
+      isDecimalNumber = true;
+    }
+  } catch (SyntaxError) {
+    result = "Invalid";
+    resetStatus = true;
+  } finally {
+    displayText.value = result;
+    digitCount = displayText.value.length - 1;
+  }
 }
-function calc() {}
+
 function btnInput(value) {
-  let tempValue = "";
+  if (resetStatus) {
+    btnClear();
+  }
   switch (value) {
     case "+":
-      break;
     case "-":
-      break;
     case "*":
-      break;
     case "/":
+      let array = ["+", "-", "*", "/"];
+      array.forEach((operators) => {
+        if (displayText.value.endsWith(operators) || displayText.value.endsWith(".")) {
+          displayText.value = displayText.value.slice(0, displayText.value.length - 1);
+          digitCount--;
+        }
+      });
+      displayText.value += value;
+      isDecimalNumber = false;
+      digitCount = 0;
       break;
     case ".":
-      break;
-    case 0:
-      if (tempValue.startsWith("0",0)) {
+      if (isDecimalNumber) {
         break;
       }
-      tempValue += value;
+      if (digitCount == 0 && displayText.value != "0") {
+        displayText.value += "0";
+        digitCount++;
+      }
+      displayText.value += ".";
+      isDecimalNumber = true;
+      digitCount++;
+      break;
+    case 0:
+      if (displayText.value[displayText.value.length - 1] == "0" && digitCount <= 1) {
+        break;
+      }
+      displayText.value += "0";
+      digitCount++;
       break;
     default:
-      tempValue += value;
+      if (displayText.value == "0") {
+        displayText.value = value;
+      } else {
+        displayText.value += value;
+      }
+      digitCount++;
       break;
   }
-  displayText
-  display();
 }
+
 function btnDelete() {
-  displayText = displayText.slice(0, displayText.length - 1);
-  display();
+  if (displayText.value.length > 1) {
+    if (digitCount == 0) {
+      return;
+    }
+    if (displayText.value[displayText.value.length - 1] == ".") {
+      isDecimalNumber = false;
+    }
+    displayText.value = displayText.value.slice(0, displayText.value.length - 1);
+    digitCount--;
+  } else {
+    btnClear();
+  }
 }
+
 function btnClear() {
-  displayText = "0";
-  display();
+  displayText.value = "0";
+  resetStatus = false;
+  isDecimalNumber = false;
+  digitCount = 0;
 }
+
 function changeTheme() {
-  let currentTheme = document.getElementById("pageTheme").getAttribute("href");
-  if (currentTheme == darkTheme) {
-    document.getElementById("pageTheme").setAttribute("href", lightTheme);
+  let pageTheme = document.getElementById("pageTheme");
+  if (pageTheme.getAttribute("href") == darkTheme) {
+    pageTheme.setAttribute("href", lightTheme);
     document.getElementById("btnTheme").innerHTML = "☀️";
   } else {
-    document.getElementById("pageTheme").setAttribute("href", darkTheme);
+    pageTheme.setAttribute("href", darkTheme);
     document.getElementById("btnTheme").innerHTML = "🌙";
   }
 }
